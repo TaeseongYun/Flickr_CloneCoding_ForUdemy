099eb50a429155694afe97031fce1e89b944a805
TaeseongYun <yts0646@naver.com>
2019-03-22 21:14:50 +0900

    1 : ADD_Fragment(Camera,Home,More)

---
+++ app/src/main/java/tsdev/tech/util/Util.kt
@@ -0,0 +1,7 @@
+package tsdev.tech.util
+
+import android.support.annotation.IdRes
+import android.support.v7.app.AppCompatActivity
+
+fun AppCompatActivity.replace(@IdRes frameId: Int, fragment: android.support.v4.app.Fragment, tag: String? = null) =
+        supportFragmentManager.beginTransaction().replace(frameId, fragment, tag).commit()
---
+++ app/src/main/java/tsdev/tech/view/main/HomeFragment.kt
@@ -0,0 +1,25 @@
+package tsdev.tech.view.main
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import tsdev.tech.R
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        val KEY_TITLE = "key-title"
+    }
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater?.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        message.setText(arguments?.getInt(KEY_TITLE) ?: 0)
+    }
+}
